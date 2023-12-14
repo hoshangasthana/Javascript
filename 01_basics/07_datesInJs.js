b98e78c887b69e5745439140dfd86b91c4b4f89a
hoshang2900 <hoshang0291@gmail.com>
2023-12-14 12:54:21 +0000

    date in Javascript

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,33 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString);
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-24")
+// let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleDateString);
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+
+    
+})
+
