5bf2f6f8abd2829abb9985abadd5f2c7eded8fed
hoshang2900 <hoshang0291@gmail.com>
2023-12-14 17:23:37 +0000

    Objects in Javascript

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,55 @@
+// singleton => jab hum "constuctor" ke through object banate
+            //   hai to jab apne type ka 1 hi instance jab banate hai
+// Object.create
+
+
+
+
+
+// Object "Literals"
+const JsUser = {
+    name: "Hoshang",
+    "full name": "Hoshang Asthana",
+    age: 23,
+    location: "Jodhpur",
+    email: "hoshang0291@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Mondays", "Saturday"]
+}
+
+// console.log(JsUser.email);  //hoshang0291@gmail.com
+
+// // console.log(JsUser[email]); // ERROR
+// console.log(JsUser["email"]);  //hoshang0291@gmail.com
+
+// // console.log(JsUser."full name"); //Error=> chance hi nhi hum aise type ko access kar paye that's why we have this second method to access
+// console.log(JsUser["full name"]); //Hoshang Asthana
+
+
+
+// //INTERVIEW QUESTION
+// // how to declare a symbol like a key in object
+// // Declaring a Symbol (one of 7 primitive data types)
+// const mySym = Symbol("key1");
+// const JsUser = {
+//     [mySym]: "key1",
+//     name: "Hoshang",
+//     "full name": "Hoshang Asthana",
+//     age: 23,
+//     location: "Jodhpur",
+//     email: "hoshang0291@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Mondays", "Saturday"]
+// }
+// console.log(mySym); //Symbol(key1)
+// console.log(JsUser[mySym]);  // key1
+
+
+JsUser.email =  "hoshang@chatgpt.com"
+console.log(JsUser["email"])
+
+Object.freeze(JsUser)
+JsUser.email =  "hoshang@chatgpt.com"
+console.log(JsUser["email"])
+
+
