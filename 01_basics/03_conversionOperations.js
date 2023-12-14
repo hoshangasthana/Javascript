f243a75a6f58dbafd24c473c945f102597de77af
hoshang2900 <hoshang0291@gmail.com>
2023-12-14 10:02:50 +0000

    Operations in Javascript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,12 +1,12 @@
 score = "3563"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN(Not a Number)
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1 => ture; 0=> false
 // "" => false
 // "asdg" => true
@@ -23,5 +23,39 @@ console.log(booleanIsLoggedIn);
 let someNumber = 34
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************ Operations ********************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hoshang"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+// study about prefix and postfix on mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
